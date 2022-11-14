1f5aeb4cd24bcb52d0beac939c34b4b1651b1323
7d <61975820+iiiii7d@users.noreply.github.com>
2022-11-14 22:40:46 +0800

    changelog & bump version no

--- statustracker-client/src/sections/Info.svelte
+++ statustracker-client/src/sections/Info.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import SvelteMarkdown from "svelte-markdown";
   import source from "../../../README.md?raw";
+  import source2 from "../../../changelog.md?raw";
 </script>
 
-<SvelteMarkdown {source}></SvelteMarkdown>
+<SvelteMarkdown {source} />
+<hr>
+<SvelteMarkdown source={source2} />
