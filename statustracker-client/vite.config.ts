61c2e3eabab307b8cf82a0d93fbfbccb741213ec
7d <61975820+iiiii7d@users.noreply.github.com>
2022-11-14 19:51:17 +0800

    attempt to add autoprefixer

--- statustracker-client/vite.config.ts
+++ statustracker-client/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import topLevelAwait from "vite-plugin-top-level-await";
+import autoprefixer from "autoprefixer";
+import postcssScss from "postcss-scss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), topLevelAwait()],
+  plugins: [svelte()],
   base: "/statustracker2/",
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
+  css: {
+    postcss: {
+      syntax: postcssScss,
+      plugins: [autoprefixer({})],
+    },
   },
 });
